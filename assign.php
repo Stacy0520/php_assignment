e132e8b83aca5f9e8b37ca0c4726319ce830680b
_Tess Cee <stacymonica543@gmail.com>
2023-06-22 08:35:14 +0300

    assignment

--- assign.php
+++ assign.php
@@ -8,8 +8,10 @@ white, green, red,
  white
 */
 $color=["white","green","red"];
-sort($color);
-echo implode($colors);
+asort($color);
+foreach($color as $value){
+    echo("$value <br>"); 
+}
 
 /*2. $x = array(1, 2, 3, 4, 5);
 Delete an element from the above PHP array. After deleting the element, 
@@ -18,7 +20,12 @@ Sample Output :
 array(5) { [0]=> int(1) [1]=> int(2) [2]=> int(3) [3]=> int(4) [4]=> int(5) }
 array(4) { [0]=> int(1) [1]=> int(2) [2]=> int(3) [3]=> int(5) }*/
 
-
+$x=array(1,2,3,4,5);
+var_dump($x);
+echo("<br>");
+unset($x[3]);
+var_dump($x);
+echo("<br>");
 
 
 
@@ -29,7 +36,12 @@ Original array :
 1 2 3 4 5
 After inserting '$' the array is :
 1 2 3 $ 4 5*/
-
+$org=array(1,2,3,4,5);
+echo implode($org);
+echo("<br>");
+array_splice($org,3,0,"$");
+echo implode($org);
+echo("<br>");
 
 
 
@@ -41,17 +53,43 @@ Expected Output :
 Average Temperature is : 70.6
 List of seven lowest temperatures : 60, 62, 63, 63, 64,
 List of seven highest temperatures : 76, 78, 79, 81, 85,*/
-$a=7;
-echo $a;
+
+$temp=[78, 60, 62, 68, 71, 68, 73, 85, 66, 64, 76, 63, 75, 
+76, 73, 68, 62, 73, 72, 65, 74, 62, 62, 65, 64, 68, 73, 75, 79, 73];
+$sum=array_sum($temp);
+
+$number=count($temp);
+$average=$sum / $number;
+echo("average temperature is: $average<br>");
+sort($temp);
+
+$lowest7=array_slice($temp,0 ,7);
+echo implode(" ",$lowest7);
+echo("<br>");
+
+$highest7=array_slice($temp,23,30);
+echo implode(" ",$highest7);
+echo "<br>";
 
 
 
 /*5. Create a script that displays 1-2-3-4-5-6-7-8-9-10 on one line. There will be 
 no hyphen(-) at starting and ending position.*/
 
+for($a=1; $a<10; $a++);
+if ($a<10){
+    echo "$a-";
+}else{
+    echo "$a". "\n";
+}
+
 
 /*6. Create a script using a for loop to add all the integers between 0 and 30 
 and display the total. */
 
-
+$total=0;
+for($i=0; $i<=30; $i++){
+    $total+=$i;
+}
+echo "$total";
 
